2477573cf98a883aa2f90cd1d14c5f18674df130
Alex Karavil <alex@karavil.io>
2020-04-08 19:00:39 -0400

    Completed user router for requests on /users

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code away!
+const express = require("express");
+
+const userRouter = require("./users/userRouter");
+const postRouter = require("./users/userRouter");
+
+const app = express();
+app.use(express.json());
+
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+
+app.get("/", (req, res) => {
+   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+app.listen(5000);
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,127 @@
-const express = require('express');
+const express = require("express");
+const userDB = require("./userDb");
+const postDB = require("../posts/postDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post("/", validateUser, (req, res) => {
+   userDB
+      .insert(req.body)
+      .then((newUser) => {
+         res.status(201).json(newUser);
+      })
+      .catch((err) => {
+         res.status(500).json("Weird error...");
+      });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
+   postDB
+      .insert({ ...req.body, user_id: req.params.id })
+      .then((id) => {
+         res.status(201).json(id);
+      })
+      .catch((err) => {
+         res.status(500).json("Weird error...");
+      });
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", (req, res) => {
+   userDB
+      .get()
+      .then((users) => {
+         res.status(200).json(users);
+      })
+      .catch((err) => {
+         res.status(500).json("Weird error...");
+      });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", validateUserId, (req, res) => {
+   userDB
+      .getById(req.params.id)
+      .then((user) => {
+         res.status(200).json(user);
+      })
+      .catch((err) => {
+         console.log(err);
+         res.status(500).json("Weird error...");
+      });
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get("/:id/posts", validateUserId, (req, res) => {
+   userDB
+      .getUserPosts(req.params.id)
+      .then((posts) => {
+         res.status(200).json(posts);
+      })
+      .catch((err) => {
+         res.status(500).json("Weird error...");
+      });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", validateUserId, (req, res) => {
+   userDB
+      .remove(req.params.id)
+      .then((count) => {
+         res.status(200).json({ message: "User deleted" });
+      })
+      .catch((err) => {
+         res.status(500).json("Weird error...");
+      });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put("/:id", validateUserId, validateUser, (req, res) => {
+   userDB
+      .update(req.params.id, req.body)
+      .then((count) => {
+         if (count) {
+            res.status(200).json({ message: "User updated" });
+         }
+      })
+      .catch((err) => {
+         console.log(err);
+         res.status(500).json({ message: "Weird error..." });
+      });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+   userDB
+      .getById(req.params.id)
+      .then((user) => {
+         if (user) {
+            next();
+         } else {
+            res.status(404).json({ message: "User id is not valid." });
+         }
+      })
+      .catch((err) => {
+         res.status(500).json(
+            "Weird error... make sure to have an ID in your request parameters."
+         );
+      });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+   if (req.body.name && req.body.name.length > 0) {
+      next();
+   } else {
+      res.status(400).json({
+         message: "Please make sure to input a name for the user.",
+      });
+   }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+   if (req.body.name && req.body.name.length > 0) {
+      next();
+   } else {
+      res.status(400).json({
+         message: "Please make sure to input a text for the post.",
+      });
+   }
 }
 
 module.exports = router;
